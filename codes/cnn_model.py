79ade8463270c8280927940bacf3b1fe00c7e38e
bernarduwineza <bernarduwineza@gmail.com>
2019-05-24 09:34:38 -0700

    initial commit

---
+++ codes/cnn_model.py
@@ -0,0 +1,60 @@
+########################################################################
+# 2. DEFINE YOUR CONVOLUTIONAL NEURAL NETWORK
+########################################################################
+
+import torch.nn as nn
+import torch.nn.functional as F
+
+
+class ConvNet(nn.Module):
+    def __init__(self, init_weights=False):
+        super(ConvNet, self).__init__()
+        self.conv1 = nn.Sequential(
+            nn.Conv2d(3, 12, kernel_size=2, stride=2, padding=0),
+            nn.ReLU()
+        )
+        self.conv2 = nn.Sequential(
+            nn.Conv2d(12, 48, kernel_size=2, stride=2, padding=0),
+            nn.ReLU(),
+            nn.MaxPool2d(kernel_size=2, stride=2)
+        )
+        self.conv3 = nn.Sequential(
+            nn.Conv2d(48, 48, kernel_size=2, stride=2, padding=2),
+            nn.ReLU()
+        )
+        self.conv4 = nn.Sequential(
+            nn.Conv2d(48, 12, kernel_size=1, stride=1, padding=2),
+            nn.ReLU()
+        )
+        self.drop_out = nn.Dropout()
+        self.fc1 = nn.Linear(8*8*12, 100)
+        self.fc2 = nn.Linear(100, 10)
+        
+        if init_weights:
+            self._initialize_weights()
+
+    def forward(self, x):
+        out = self.conv1(x)
+        out = self.conv2(out)
+        out = self.conv3(out)
+        out = self.conv4(out)
+        out = out.reshape(out.size(0), -1)
+
+        out = self.drop_out(out)
+        out = self.fc1(out)
+        out = self.fc2(out)
+        return out
+
+    def _initialize_weights(self):
+        for m in self.modules():
+            if isinstance(m, nn.Conv2d):
+                nn.init.kaiming_normal_(m.weight, mode='fan_out', nonlinearity='relu')
+                if m.bias is not None:
+                    nn.init.constant_(m.bias, 0)
+            elif isinstance(m, nn.BatchNorm2d):
+                nn.init.constant_(m.weight, 1)
+                nn.init.constant_(m.bias, 0)
+            elif isinstance(m, nn.Linear):
+                nn.init.normal_(m.weight, 0, 0.01)
+                nn.init.constant_(m.bias, 0)
+
